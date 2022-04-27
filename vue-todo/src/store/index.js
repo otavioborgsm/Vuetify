3322ceb2d0a9b5e75c43190089cb9491e46789c4
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-04-27 11:35:41 -0300

    desenvolvendo aplicação

---
+++ vue-todo/src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tarefas:[
+      {id:1, titulo:'Ir ao mercado' , concluido: false}
+    ]
+  },
+  getters: {
+  },
+  mutations: {
+    adicionaTarefa(state, titulo){
+      if(titulo){
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false
+        })
+        this.campoInput= null
+      }
+    },
+    removeTarefa(state, id){
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ vue-todo/src/views/Tarefas.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-col
+      cols="12"
+    >
+      <v-text-field
+        v-model="campoInput"
+        label="Qual sua tarefa?"
+        outlined
+        clearable
+        @keyup.enter="handleAddTarefa"
+      ></v-text-field>
+    </v-col>
+
+    <v-list
+      flat
+      subheader
+    >
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+      <div 
+        v-for="tarefa, index in $store.state.tarefas"
+        :key="index"
+      >
+        <TarefaItem 
+          :tarefa="tarefa"
+        />
+      </div>
+      
+
+
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import TarefaItem from '../components/tarefas/TarefaItem.vue'
+
+  export default {
+    name: 'TarefasTodo',
+
+    components:{
+      TarefaItem
+    },
+    data(){
+      return{
+        campoInput:null,
+        
+      }
+    },
+    methods:{
+      handleAddTarefa(){
+        if(this.campoInput){
+          this.$store.commit("adicionaTarefa", this.campoInput)
+          this.campoInput= null
+        }
+      }
+    }
+  }
+</script>
